a53d44ecd4253d082b482f5831d973eede801f96
CristianGmz7 <cristiangomez.cagg78@gmail.com>
2025-03-13 12:51:40 -0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='text-3xl text-blue-400'>index</Text>
+      {/* redireccionar a la pagina principal, si hay problemas de stack o algo estar reiniciasndo termianl*/}
+    </View>
+  )
+}
+
+export default index
